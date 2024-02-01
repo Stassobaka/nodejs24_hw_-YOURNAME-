f4363303cd488c9d6b550727e9243ce25f1c8c2a
evhenious <evhenious@gmail.com>
2024-02-01 09:50:22 +0200

    your changes to logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,8 +1,30 @@
+require('dotenv').config();
+const chalk = require('chalk');
+
 function logger(moduleName) {
+
+
     return {
-        info: (...message) => console.log(moduleName, ...message),
-        warn: (...message) => console.warn(moduleName, ...message),
-        error: (...message) => console.error(moduleName, ...message),
-    };
+        info: function (...message){
+            if (process.env.LOG_LEVEL === 'info') {
+                process.env.COLORS_ENABLED === '1' ?  console.log (chalk.blue(moduleName), ...message) : console.log(moduleName, ...message);
+                this.warn;
+                this.error;
+            }
+        },
+
+        warn: function (...message){
+            if (process.env.LOG_LEVEL === 'warn' || process.env.LOG_LEVEL === 'info' ) {
+                process.env.COLORS_ENABLED === '1' ?  console.log (chalk.yellow(moduleName), ...message) : console.log(moduleName, ...message);
+                this.error;
+            }
+        },
+
+        error: function (...message){
+            if (process.env.LOG_LEVEL === 'error' || process.env.LOG_LEVEL === 'warn' || process.env.LOG_LEVEL === 'info' ) {
+                process.env.COLORS_ENABLED === '1' ?  console.log (chalk.red(moduleName), ...message) : console.log(moduleName, ...message);
+            }
+        },
+    }
 }
 module.exports = logger;
