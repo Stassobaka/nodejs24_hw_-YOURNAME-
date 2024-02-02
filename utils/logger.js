09f9f28b283a5a692683482f76f4c841e823d9f1
Stassobaka <proizvodstvo.stas@gmail.com>
2024-02-02 21:47:49 +0100

    change warn

--- utils/logger.js
+++ utils/logger.js
@@ -11,18 +11,30 @@ function logger(moduleName) {
                 this.warn;
                 this.error;
             }
+            if(process.env.LOG_LEVEL === undefined){
+                process.env.COLORS_ENABLED === '1' ?  console.log (chalk.blue(moduleName), ...message) : console.log(moduleName, ...message);
+                this.error;
+            }
         },
 
         warn: function (...message){
             if (process.env.LOG_LEVEL === 'warn' || process.env.LOG_LEVEL === 'info' ) {
                 process.env.COLORS_ENABLED === '1' ?  console.log (chalk.yellow(moduleName), ...message) : console.log(moduleName, ...message);
                 this.error;
-            }   
+            }
+            if(process.env.LOG_LEVEL === undefined){
+                process.env.COLORS_ENABLED === '1' ?  console.log (chalk.yellow(moduleName), ...message) : console.log(moduleName, ...message);
+                this.error;
+            }
         },
 
         error: function (...message){
             if (process.env.LOG_LEVEL === 'error' || process.env.LOG_LEVEL === 'warn' || process.env.LOG_LEVEL === 'info' ) {
                 process.env.COLORS_ENABLED === '1' ?  console.log (chalk.red(moduleName), ...message) : console.log(moduleName, ...message);
+            }
+            if(process.env.LOG_LEVEL === undefined){
+                process.env.COLORS_ENABLED === '1' ?  console.log (chalk.red(moduleName), ...message) : console.log(moduleName, ...message);
+                this.error;
             }
         },
     }
