bda2d49f9aef08eea39926d3569ee752c6ecaf76
Stassobaka <proizvodstvo.stas@gmail.com>
2024-02-02 21:31:43 +0100

    fix erorr

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 const logger = require('./utils/logger')('main');
-<<<<<<< HEAD
 
 logger.info('This is info');
 logger.error('This is Error');
 logger.warn('This is Warn');
-=======
-logger.info('the script is running!');
-logger.error('Error','Error','Error');
->>>>>>> f4363303cd488c9d6b550727e9243ce25f1c8c2a
