a309e7aebd5126a36ee7acc9c1257a5cfb742e29
evhenious <evhenious@gmail.com>
2024-02-03 14:58:57 +0200

    you don't need it

--- utils/logger.js
+++ utils/logger.js
@@ -10,10 +10,6 @@ function logger(moduleName) {
             if (logLevel === 'info') {
                 colorsEnabled ?  console.log (chalk.blue(moduleName), ...message) : console.log(moduleName, ...message);
             }
-            if(process.env.LOG_LEVEL === undefined){
-                this.warn;
-                this.error;
-            }
             if(logLevel === undefined){
                 colorsEnabled ?  console.log (chalk.blue(moduleName), ...message) : console.log(moduleName, ...message);
             }
