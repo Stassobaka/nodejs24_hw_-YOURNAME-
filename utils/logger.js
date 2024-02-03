5c82f6d243150f5686d1fcca06fd5c98b7875e34
evhenious <evhenious@gmail.com>
2024-02-03 13:47:27 +0200

    simplified env var access

--- utils/logger.js
+++ utils/logger.js
@@ -1,42 +1,44 @@
 const chalk = require('chalk');
 
 function logger(moduleName) {
+    const logLevel = process.env.LOG_LEVEL;
+    const colorsEnabled = process.env.COLORS_ENABLED === '1';
 
 
     return {
         info: function (...message){
-            if (process.env.LOG_LEVEL === 'info') {
-                process.env.COLORS_ENABLED === '1' ?  console.log (chalk.blue(moduleName), ...message) : console.log(moduleName, ...message);
+            if (logLevel === 'info') {
+                colorsEnabled ?  console.log (chalk.blue(moduleName), ...message) : console.log(moduleName, ...message);
                 this.warn;
                 this.error;
             }
-            if(process.env.LOG_LEVEL === undefined){
-                process.env.COLORS_ENABLED === '1' ?  console.log (chalk.blue(moduleName), ...message) : console.log(moduleName, ...message);
+            if(logLevel === undefined){
+                colorsEnabled ?  console.log (chalk.blue(moduleName), ...message) : console.log(moduleName, ...message);
                 this.error;
             }
         },
 
         warn: function (...message){
-            if (process.env.LOG_LEVEL === 'warn' || process.env.LOG_LEVEL === 'info' ) {
-                process.env.COLORS_ENABLED === '1' ?  console.log (chalk.yellow(moduleName), ...message) : console.log(moduleName, ...message);
+            if (logLevel === 'warn' || logLevel === 'info' ) {
+                colorsEnabled ?  console.log (chalk.yellow(moduleName), ...message) : console.log(moduleName, ...message);
                 this.error;
             }
-            if(process.env.LOG_LEVEL === undefined){
-                process.env.COLORS_ENABLED === '1' ?  console.log (chalk.yellow(moduleName), ...message) : console.log(moduleName, ...message);
+            if(logLevel === undefined){
+                colorsEnabled ?  console.log (chalk.yellow(moduleName), ...message) : console.log(moduleName, ...message);
                 this.error;
             }
         },
 
         error: function (...message){
-            if (process.env.LOG_LEVEL === 'error' || process.env.LOG_LEVEL === 'warn' || process.env.LOG_LEVEL === 'info' ) {
-                process.env.COLORS_ENABLED === '1' ?  console.log (chalk.red(moduleName), ...message) : console.log(moduleName, ...message);
+            if (logLevel === 'error' || logLevel === 'warn' || logLevel === 'info' ) {
+                colorsEnabled ?  console.log (chalk.red(moduleName), ...message) : console.log(moduleName, ...message);
             }
-            if(process.env.LOG_LEVEL === undefined){
-                process.env.COLORS_ENABLED === '1' ?  console.log (chalk.red(moduleName), ...message) : console.log(moduleName, ...message);
+            if(logLevel === undefined){
+                colorsEnabled ?  console.log (chalk.red(moduleName), ...message) : console.log(moduleName, ...message);
                 this.error;
             }
         },
     }
-} 
+}
 module.exports = logger;
 
