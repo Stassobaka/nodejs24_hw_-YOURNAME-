3f44a5a8608bfacc4d1031813718e8a9601875c2
Stassobaka <proizvodstvo.stas@gmail.com>
2024-02-06 00:44:53 +0100

    Try fix clean

--- return_directory.js
+++ return_directory.js
@@ -13,25 +13,27 @@ const absolutePathTarget = path.resolve(target);
 
 function deleteDirectory(directoryToDelete){
     fs.rmdir(directoryToDelete,(err, result)=>{
+        
         if(result){
             logger.info(`Директория ${directoryToDelete} удалена`)
         }
         if(err){
             fs.readdir(directoryToDelete, (err, result)=>{
                 if(err){
-                    logger.error('Не найдена директория')  
+                    findFilesToDelete(absoluteDirectory)
                 }else{
+                    console.log(result)
                     result.forEach((item)=>{               
-                        fs.stat(path.join(directoryToDelete, item), (err, stats)=>{
+                        fs.stat( directoryToDelete, (err, stats)=>{
                             if(err){
                                 logger.error('Ошибка содержания деректории', err) 
                                 }else {
                                     if(stats.isDirectory()){
-                                        fs.rmdir(path.resolve(item), (err)=>{
+                                        fs.rmdir(item, (err)=>{
                                             if(err){
                                                 logger.warn('Директория не пустая')
-                                                findFilesToDelete(directoryToDelete)
-                                                console.log(path.resolve(directoryToDelete))
+                                                   deleteDirectory( item)
+                                                console.log(directoryToDelete)
                                             }else{
                                                 logger.info(`Директория ${path.resolve(item)}`)
                                             }
@@ -43,6 +45,7 @@ function deleteDirectory(directoryToDelete){
                                                 logger.error(`Не удалось удалить файл ${path.resolve(item)}`)
                                             }else{
                                                 logger.info(`Файл ${path.resolve(item)} удален`)
+                                                deleteDirectory(path.join(directoryToDelete, item))
                                             }
                                         })
                                     }
@@ -58,10 +61,7 @@ function deleteDirectory(directoryToDelete){
         }
 
     })
-
-
-   
-    }
+ }
 
 
 
@@ -72,25 +72,10 @@ function findFilesToDelete(directory){
                 logger.error('Не найдена директория')
             }else{
                 if (result.includes(source)) {
-                    fs.rmdir(path.resolve(source), (err)=>{
-                        if(err){
-                            deleteDirectory(source) 
-                        }else{
-                            logger.info(`${source} удалено`)
-                        }
-
-                    })
-                      
+                    deleteDirectory(source)  
                 }
                 if (result.includes(target)) {
-                    fs.rmdir(path.resolve(target), (err)=>{
-                        if(err){
-                            deleteDirectory(target) 
-                        }else{
-                            logger.info(`${target} удалено`)
-                        }
-
-                    })
+                    deleteDirectory(target) 
                 }else{
                     logger.info('Все папки удалены')
                 }
@@ -102,32 +87,37 @@ function findFilesToDelete(directory){
 
 }
 
-
-
 findFilesToDelete(absoluteDirectory)
     },
 
     default:  function(){
    
-        const files = ['E:\\nodejs24_hw_YOURNAME\\source\\.gitkeep',
-        'E:\\nodejs24_hw_YOURNAME\\target\\',
-        'E:\\nodejs24_hw_YOURNAME\\target\\.gitkeep',
-        'E:\\nodejs24_hw_YOURNAME\\source\\subdir\\.gitkeep',
-        'E:\\nodejs24_hw_YOURNAME\\source\\.file_1',
-        'E:\\nodejs24_hw_YOURNAME\\source\\.file_2',
-        'E:\\nodejs24_hw_YOURNAME\\source\\subdir\\.file_3',
-        'E:\\nodejs24_hw_YOURNAME\\target\\.file_2',
-    
-    ]
-        
-        files.forEach((element)=>{
-            fs.writeFile(element , '', (err, result)=>{
-                if(err) console.log('error write file')
-                if(result)console.log('Done write file')
-            })
+        fs.mkdir(path.join('.', 'source'), (err)=>{
+            if(err){
+                logger.warn(`Не удалось создать папку ${path.join('.', 'source')} или она уже существует `)
+            }else{
+                logger.info(`${path.join('.', 'source')} папка создана`)
+                fs.writeFile()
+            }
         })
-       
-     
+        fs.mkdir(path.join('.', 'target'), (err)=>{
+            if(err){
+                logger.warn(`Не удалось создать папку ${path.join('.', 'target')} или она уже существует `)
+            }else{
+                logger.info(`${path.join('.', 'target')} папка создана`)
+            }
+        })
+        fs.mkdir(path.join('.', 'source', 'subdir'), (err)=>{
+            if(err){
+                logger.warn(`Не удалось создать папку ${path.join('.', 'source', 'subdir')} или она уже существует `)
+            }else{
+                logger.info(`${path.join('.', 'source', 'subdir')} папка создана`)
+            }
+        })
+
+
+
+
 }
 }
 
