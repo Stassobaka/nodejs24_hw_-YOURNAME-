55185029e01a672884ed3f15029766453637eadc
Stassobaka <proizvodstvo.stas@gmail.com>
2024-02-07 23:05:06 +0100

    End task homework_3 fix all bags

--- return_directory.js
+++ return_directory.js
@@ -8,8 +8,6 @@ const returnDefault = {
         const source  = 'source';
         const target = 'target';
         const absoluteDirectory = '.'
-const absolutePathSource = path.resolve(source);
-const absolutePathTarget = path.resolve(target);
 
 function deleteDirectory(directoryToDelete){
     fs.rmdir(directoryToDelete,(err, result)=>{
@@ -23,35 +21,36 @@ function deleteDirectory(directoryToDelete){
                     findFilesToDelete(absoluteDirectory)
                 }else{
                     result.forEach((item)=>{  
-                        fs.stat(path.join(directoryToDelete, item) , (err, stats)=>{
-                            if(err){
-                                logger.error('Ошибка содержания деректории', err) 
-                                }else {
-                                    if(stats.isDirectory()){
-                                        fs.rmdir(item, (err)=>{
-                                            if(err){
-                                                logger.warn('Директория не пустая')
-                                                   deleteDirectory( path.join(directoryToDelete, item))
-                                                   findFilesToDelete(absoluteDirectory)
-                                            }else{
-                                                logger.info(`Директория ${path.resolve(item)}`)
-                                            }
-                                        })
-                                    }
-                                    if(stats.isFile()){
-                                        fs.unlink(path.join(directoryToDelete, item), (err)=>{
-                                            if(err){
-                                                logger.error(`Не удалось удалить файл ${path.resolve(item)}`)
-                                            }else{
-                                                logger.info(`Файл ${path.resolve(item)} удален`)
-                                                deleteDirectory(path.join(directoryToDelete, item))
-                                            }
-                                        })
-                                    }
+
+                            fs.stat(path.join(directoryToDelete, item) , (err, stats)=>{
+                                if(err){
+                                    logger.warn('Отсутствует директория', err.path) 
+                                    }else {
+                                        if(stats.isDirectory()){
+                                            fs.rmdir(item, (err)=>{
+                                                if(err){                                              
+                                                       deleteDirectory( path.join(directoryToDelete, item))
+                                                }else{
+                                                    logger.info(`Директория ${path.resolve(item)}`)
+                                                }
+                                            })
+                                        }
+                                        if(stats.isFile()){
+                                            fs.unlink(path.join(directoryToDelete, item), (err)=>{
+                                                if(err){
+                                                    logger.error(`Не удалось удалить файл ${path.resolve(item)}`)
+                                                }else{
+                                                    logger.info(`Файл ${path.resolve(item)} удален`)
+                                                    deleteDirectory(path.join(directoryToDelete, item))
+                                                }
+                                            })
+                                        }
+                                            
                                         
-                                    
-                                }
-                            })
+                                    }
+                                })
+                        
+
                         })
                         
                     }
@@ -75,10 +74,7 @@ function findFilesToDelete(directory){
                 }
                 if (result.includes(target)) {
                     deleteDirectory(target) 
-                }else{
-                    logger.info('Все папки удалены')
-                }
-            
+                }          
             }
 
     })
@@ -91,30 +87,91 @@ findFilesToDelete(absoluteDirectory)
 
     default:  function(){
    
-        fs.mkdir(path.join('.', 'source'), (err)=>{
+
+
+        function addDirectory(){
+            fs.mkdir(path.join('.', 'source'), (err)=>{
+                if(err){
+                    logger.warn(`Не удалось создать папку ${path.join('.', 'source')} или она уже существует `)
+                }else{
+                    logger.info(`${path.join('.', 'source')} папка создана`)
+                }
+            })
+            fs.mkdir(path.join('.', 'target'), (err)=>{
+                if(err){
+                    logger.warn(`Не удалось создать папку ${path.join('.', 'target')} или она уже существует `)
+                }else{
+                    logger.info(`${path.join('.', 'target')} папка создана`)
+                }
+            })
+            let subdir = path.join('.', 'source', 'subdir')
+            fs.mkdir(subdir , (err)=>{
+                if(err){
+                    logger.warn(`Не удалось создать папку ${path.join('.', 'source', 'subdir')} или она уже существует `)
+                }else{
+                    logger.info(`${path.join('.', 'source', 'subdir')} папка создана`)
+                }
+            })
+            addFile()
+        }
+
+       function addFile(){
+        fs.readdir('.', (err, result)=>{
             if(err){
-                logger.warn(`Не удалось создать папку ${path.join('.', 'source')} или она уже существует `)
+                logger.error(' Не удалось прочитать дерево')
             }else{
-                logger.info(`${path.join('.', 'source')} папка создана`)
-                fs.writeFile()
-            }
-        })
-        fs.mkdir(path.join('.', 'target'), (err)=>{
-            if(err){
-                logger.warn(`Не удалось создать папку ${path.join('.', 'target')} или она уже существует `)
-            }else{
-                logger.info(`${path.join('.', 'target')} папка создана`)
-            }
-        })
-        fs.mkdir(path.join('.', 'source', 'subdir'), (err)=>{
-            if(err){
-                logger.warn(`Не удалось создать папку ${path.join('.', 'source', 'subdir')} или она уже существует `)
-            }else{
-                logger.info(`${path.join('.', 'source', 'subdir')} папка создана`)
+                logger.info(result)
+
+                result.forEach((item)=>{
+                    if(item === 'source'){
+                        let file_1 = path.join('source', 'file_1' )
+                        let file_2 = path.join('source', 'file_2' )
+                        let file_3 = path.join('source', 'subdir', 'file_3' )
+                        
+                        fs.writeFile(file_1, '', (err)=>{
+                            if(err){
+                                logger.error(`Файл ${file_1} не записан`)
+                            }else{
+                                logger.info(`Файл file_1 записан в папку ${file_1}`)
+                            }                           
+                        })
+                        
+                        fs.writeFile(file_2, '', (err)=>{
+                            if(err){
+                                logger.error(`Файл ${file_2} не записан`)
+                            }else{
+                                logger.info(`Файл file_3 записан в папку ${file_2}`)
+                            }                           
+                        })
+
+                        fs.writeFile(file_3, '', (err)=>{
+                            if(err){
+                                logger.error(`Файл ${file_3} не записан`)
+                            }else{
+                                logger.info(`Файл file_2 записан в папку ${file_3}`)
+                            }                           
+                        })
+
+                    }
+                    if(item === 'target'){
+                        let file_2 = path.join('target', 'file_2' )
+                        fs.writeFile(file_2, '', (err)=>{
+                            if(err){
+                                logger.error(`Файл ${file_2} не записан`)
+                            }else{
+                                logger.info(`Файл file_2 записан в папку ${file_2}`)
+                            }                           
+                        })                   
+                    }
+                })
             }
         })
 
+       }
+        
 
+
+addDirectory();
 
 
 }
