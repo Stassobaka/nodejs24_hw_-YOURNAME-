ce24492cd0722698d3ec5ec79140045e465cbaba
Stassobaka <proizvodstvo.stas@gmail.com>
2024-02-09 02:08:44 +0100

    Try fix bags

--- loggerStream.js
+++ loggerStream.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
 const path = require('path');
+const {Writable} = require('stream')
+
+
 const loggerStream = {
 
 
-
-    writeStream: function (...message){
-        const info = [...message]
+    creatDirectory: function (){
+   
      
-        function creatDirectory(){
+       function  creatDirectory(){
             fs.readdir('.', (err, result)=>{
                 console.log(result)
                 if(err){
@@ -23,6 +25,7 @@ const loggerStream = {
                         
                     }else{
                         console.log(`Директория ${path.resolve('logs')} существует `)
+                        createFile()
                     }
                 }
             })
@@ -40,17 +43,87 @@ const loggerStream = {
                 if(err){
                     console.log('Файл errors не создан',err)
                 }
+
            })
+           
+           
         }
 
-        creatDirectory()
 
+creatDirectory()
+
+
+    },
+
+    writeLoggerInfo:function(...message){
+
+
+        
+        const info = [...message]
+            const logInfo = path.join('logs','info.log')
+            const writeStream = fs.createWriteStream(logInfo, {encoding: 'utf8', flags: 'a'})
+            writeStream.on('open', () => {
+        info.forEach((item) => {
+            console.log(item);
+            writeStream.write('Info:' + item + '\n', (err) => {
+                if (err) console.log('Error writing to stream:', err);
+            });
+        });
+        writeStream.on('close', () => {
+            console.log('Stream closed');
+        });
+        writeStream.end();
+        writeStream.on('error', (err) => {
+            console.log('Stream error:', err);
+        });
+        
+})
+       
+
+
+    },
+
+    writeLoggerWarn: (...message) => {
+        const info = [...message];
+        const logInfo = path.join('logs', 'errors.log');
+        const writeStream = fs.createWriteStream(logInfo, { encoding: 'utf8', flags: 'a' });
+        writeStream.on('open', () => {
+            info.forEach((item) => {
+                console.log(item);
+                writeStream.write('warn:' + item + '\n', (err) => {
+                    if (err) console.log('Error writing to stream:', err);
+                });
+            });
+            writeStream.end();
+
+        });
+
+        writeStream.on('error', (err) => {
+            console.log('Stream error:', err);
+        });
+
+    },
+
+    writeLoggerError: function(...message){
+        const info = [...message];
+        const logInfo = path.join('logs', 'errors.log');
+        
+        const writeStream = fs.createWriteStream(logInfo, { encoding: 'utf8', flags: 'a' });
+ 
+        writeStream.on('open', () => {
+            info.forEach((item) => {
+                console.log(item);
+                writeStream.write('Error:' + item + '\n', (err) => {
+                    if (err) console.log('Error writing to stream:', err);
+                });
+            });
+            writeStream.end();
+            writeStream.on('error', (err) => {
+                console.log('Stream error:', err);
+            });
+        });
+    },
 }
-
-}
-
-
-
 
 
 module.exports =loggerStream;
