f8aafc99ae18ef4f455f01cbbf713912d20a465e
evhenious <evhenious@gmail.com>
2024-02-12 21:28:16 +0200

    End task

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const logger = require('./utils/logger')('main');
 const returnDefault =  require('./return_directory')
 const fileSync = require('./fileSync')
 
-logger.info('This is info');
-// logger.error('This is Error');
-// logger.warn('This is Warn');
+logger.info('This is info','This is info','This is info');
+logger.error('This is Error');
+logger.warn('This is Warn');
 
 
 // returnDefault.clean() // Очистка папок от всех файлов к начальным условиям задачи
--- utils/logger.js
+++ utils/logger.js
@@ -1,30 +1,73 @@
 const chalk = require('chalk');
 const loggerStream = require('../loggerStream')
+const fs = require('fs');
+const path = require('path');
 
-function logger(moduleName) {
+ function logger(moduleName) {
     const logLevel = process.env.LOG_LEVEL;
     const colorsEnabled = process.env.COLORS_ENABLED === '1';
 
 
-    return {
-        info: function (...message){
-            if (logLevel === 'info') {
-                // працює тільки коли в process.env.LOG_LEVEL значення 'info'
-                colorsEnabled ?  console.log (chalk.blue(moduleName), ...message) : console.log(moduleName, ...message);
-            }
-            loggerStream.writeStream(...message)
-        },
 
-        warn: function (...message){
-            if (logLevel === 'warn' || logLevel === 'info' || logLevel === undefined) {
-                // працює коли в process.env.LOG_LEVEL значення 'info', 'warn' або нічого
+    return {
+        info:  function (...message){
+            if (logLevel === 'info') {
+                colorsEnabled ?  console.log (chalk.blue(moduleName), ...message) : console.log(moduleName, ...message);
+
+            };
+       function madeDirectory(){
+        loggerStream.creatDirectory()
+       }
+
+       setTimeout(madeDirectory,100)
+
+        function startStream(){
+            loggerStream.writeLoggerInfo(...message)
+        }
+
+        setTimeout(startStream,1000)
+    },
+
+        warn:  function (...message){
+            if (logLevel === 'warn' || logLevel === 'info' ) {
                 colorsEnabled ?  console.log (chalk.yellow(moduleName), ...message) : console.log(moduleName, ...message);
             }
+            if(logLevel === undefined){
+                colorsEnabled ?  console.log (chalk.yellow(moduleName), ...message) : console.log(moduleName, ...message);
+            }
+
+            function madeDirectory(){
+                loggerStream.creatDirectory()
+               }
+
+               setTimeout(madeDirectory,100)
+
+            loggerStream.creatDirectory()
+            function startStream(){
+                loggerStream.writeLoggerWarn(...message)
+            }
+
+            setTimeout(startStream,1000)
         },
 
         error: function (...message){
-            // працює завжди, по умовах задачі у нас немає випадків коли метод error() має бути відключений
-            colorsEnabled ?  console.log (chalk.red(moduleName), ...message) : console.log(moduleName, ...message);
+            if (logLevel === 'error' || logLevel === 'warn' || logLevel === 'info' ) {
+                colorsEnabled ?  console.log (chalk.red(moduleName), ...message) : console.log(moduleName, ...message);
+            }
+            if(logLevel === undefined){
+                colorsEnabled ?  console.log (chalk.red(moduleName), ...message) : console.log(moduleName, ...message);
+            }
+            function madeDirectory(){
+                loggerStream.creatDirectory()
+               }
+
+               setTimeout(madeDirectory,100)
+
+            function startStream(){
+                loggerStream.writeLoggerError(...message)
+            }
+            setTimeout(startStream,1000)
+
         }
     }
 }
