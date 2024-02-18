fb475b385783b6637542fe4f7ca481c261e6370d
Stassobaka <proizvodstvo.stas@gmail.com>
2024-02-18 13:57:15 +0100

    added GET and /healthcheck

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const http = require('http')
+const logger = require('./utils/logger')('main');
 
 const server = http.createServer();
 
 server.listen(3000);
 // Создали сервер 
+
+
+
+
+
+server.on('request', (req, resp) =>{
+    if(req.url === '/healthcheck' && req.method === 'GET'){
+        resp.writeHead(200, 'OK')
+        resp.write('healthcheck passed')
+        logger.info(`${req.method} ${req.url} 200`)
+        resp.end()
+    }
+    
+   
+   
+})
